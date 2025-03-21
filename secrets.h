7bcb51a6d4d1fbacdad55d1d09b2f1c8593643f9
Aaryan gupta <40055877+aaryangupta@users.noreply.github.com>
2025-03-21 15:32:30 +0530

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -2,16 +2,17 @@
  
 #define SECRET
  
-const char WIFI_SSID[] = "******";               
-const char WIFI_PASSWORD[] = "******";           
+const char WIFI_SSID[] = "****";               
+const char WIFI_PASSWORD[] = "****";           
  
 #define THINGNAME "AWS_DHT_11"
  
 int8_t TIME_ZONE = -5; //NYC(USA): -5 UTC
  
-const char MQTT_HOST[] = "aspx4m3sgkl-ats.iot.us-east-1.amazonaws.com";
+const char MQTT_HOST[] = "endpoint";
  
- 
+
+// Copy contents from Root-CA-certificate.pem here ▼
 static const char cacert[] PROGMEM = R"EOF(
 -----BEGIN CERTIFICATE-----
 
@@ -19,20 +20,17 @@ static const char cacert[] PROGMEM = R"EOF(
 )EOF";
  
  
-// Copy contents from XXXXXXXX-certificate.pem.crt here ▼
+// Copy contents from Device-certificate.pem.crt here ▼
 static const char client_cert[] PROGMEM = R"KEY(
 -----BEGIN CERTIFICATE-----
 
 -----END CERTIFICATE-----
-
 )KEY";
  
  
-// Copy contents from  XXXXXXXX-private.pem.key here ▼
+// Copy contents from  private.pem.key here ▼
 static const char privkey[] PROGMEM = R"KEY(
 -----BEGIN RSA PRIVATE KEY-----
 
------END RSA PRIVATE KEY-----
-
- 
+-----END RSA PRIVATE KEY----- 
 )KEY";
